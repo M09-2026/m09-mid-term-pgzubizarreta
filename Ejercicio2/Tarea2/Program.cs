9ae33d47bf7ee5a1e9ef8d50229b025feb516733
pgzubizarreta <paulgomezzubi@gmail.com>
2026-03-16 11:58:43 +0100

    Implemento ejercicio 2 tarea 1 con control de calidad y semáforos
    Implemento ejercicio 2 tarea 1 con control de calidad y semáforos

--- Ejercicio2/Tarea2/Program.cs
+++ Ejercicio2/Tarea2/Program.cs
@@ -1,2 +1,126 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Ejercicio2.Tarea1
+{
+    // Estados posibles del componente
+    public enum EstadoComponente
+    {
+        EsperaMecanizado,
+        EnMecanizado,
+        EsperaInspeccion,
+        EnInspeccion,
+        Completado
+    }
+
+    // Clase componente
+    public class Componente
+    {
+        public int Id { get; set; }
+        public int TiempoMecanizado { get; set; }
+        public bool RequiereInspeccion { get; set; }
+        public EstadoComponente Estado { get; set; }
+        public int OrdenLlegada { get; set; }
+
+        public Componente(int id, int tiempo, bool inspeccion, int orden)
+        {
+            Id = id;
+            TiempoMecanizado = tiempo;
+            RequiereInspeccion = inspeccion;
+            OrdenLlegada = orden;
+        }
+    }
+
+    class Program
+    {
+        // 4 estaciones de mecanizado
+        static SemaphoreSlim estaciones = new SemaphoreSlim(4);
+
+        // 2 máquinas de control de calidad
+        static SemaphoreSlim qc = new SemaphoreSlim(2);
+
+        static Random random = new Random();
+        static object lockRandom = new object();
+        static object lockConsola = new object();
+
+        static void Main(string[] args)
+        {
+            Thread[] hilos = new Thread[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                int orden = i + 1;
+                Componente c = CrearComponente(orden);
+
+                Log(c, EstadoComponente.EsperaMecanizado);
+
+                hilos[i] = new Thread(() => ProcesarComponente(c));
+                hilos[i].Start();
+
+                Thread.Sleep(2000);
+            }
+
+            for (int i = 0; i < 4; i++)
+                hilos[i].Join();
+
+            Console.WriteLine("Fin de la simulación.");
+        }
+
+        static Componente CrearComponente(int orden)
+        {
+            int id;
+            int tiempo;
+            bool inspeccion;
+
+            lock (lockRandom)
+            {
+                id = random.Next(1, 101);
+                tiempo = random.Next(5, 16);
+                inspeccion = random.Next(0, 2) == 1;
+            }
+
+            return new Componente(id, tiempo, inspeccion, orden);
+        }
+
+        static void ProcesarComponente(Componente c)
+        {
+            // Espera estación de mecanizado
+            estaciones.Wait();
+
+            c.Estado = EstadoComponente.EnMecanizado;
+            Log(c, c.Estado);
+
+            Thread.Sleep(c.TiempoMecanizado * 1000);
+
+            estaciones.Release();
+
+            if (c.RequiereInspeccion)
+            {
+                c.Estado = EstadoComponente.EsperaInspeccion;
+                Log(c, c.Estado);
+
+                qc.Wait();
+
+                c.Estado = EstadoComponente.EnInspeccion;
+                Log(c, c.Estado);
+
+                Thread.Sleep(15000);
+
+                qc.Release();
+            }
+
+            c.Estado = EstadoComponente.Completado;
+            Log(c, c.Estado);
+        }
+
+        static void Log(Componente c, EstadoComponente estado)
+        {
+            lock (lockConsola)
+            {
+                Console.WriteLine(
+                    $"Componente {c.Id}. Entrada {c.OrdenLlegada}. Estado: {estado}. QC={c.RequiereInspeccion}");
+            }
+        }
+    }
+}
--- Ejercicio2/Tarea5/obj/Debug/net10.0/Tarea5.AssemblyInfo.cs
+++ Ejercicio2/Tarea5/obj/Debug/net10.0/Tarea5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c00134f8f55475e47730e0ae00c71e392fb82b6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c93a35b2dc2a2a846bef0f4a41ca8d4f89310f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
